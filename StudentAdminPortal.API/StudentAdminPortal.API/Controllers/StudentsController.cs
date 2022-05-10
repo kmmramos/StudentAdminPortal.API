a3779989887daa6ab474b2479bb65c212cdafd1e
kmmramos <kmmramos@gmail.com>
2022-05-11 02:51:24 +0800

    End of Section 5

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -33,7 +33,7 @@ namespace StudentAdminPortal.API.Controllers
 
         [HttpGet]
         [Route("[controller]/{studentId:guid}")]
-        public async Task<IActionResult> GetStudentAsync([FromRoute] Guid studentId) 
+        public async Task<IActionResult> GetStudentAsync([FromRoute] Guid studentId)
         {
             //Fetch Student Details
             var student = await studentRepository.GetStudentAsync(studentId);
@@ -45,6 +45,23 @@ namespace StudentAdminPortal.API.Controllers
             }
 
             return Ok(mapper.Map<Student>(student));
+        }
+
+        [HttpPut]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> UpdateStudentAsync([FromRoute] Guid studentId, [FromBody] UpdateStudentRequest request)
+        {
+            if (await studentRepository.Exists(studentId))
+            {
+                //Update Details
+                var updatedStudent = await studentRepository.UpdateStudent(studentId, mapper.Map<DataModels.Student>(request));
+                
+                if(updatedStudent != null)
+                {
+                    return Ok(mapper.Map<Student>(updatedStudent));
+                }
+            }
+            return NotFound();
         }
     }
 }
--- StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StudentAdminPortal.API.Profiles.AfterMaps;
 
 namespace StudentAdminPortal.API.Profiles
 {
@@ -20,6 +21,11 @@ namespace StudentAdminPortal.API.Profiles
 
             CreateMap<DataModels.Address, Address>()
                 .ReverseMap();
+
+            CreateMap<UpdateStudentRequest, DataModels.Student>()
+                //.ForMember(dest => dest.Address.PhysicalAddress, opt => opt.MapFrom(src => src.PhysicalAddress))
+                //.ForMember(dest => dest.Address.PostalAddress, opt => opt.MapFrom(src => src.PostalAddress));
+                .AfterMap<UpdateStudentRequestAfterMap>();
         }
     }
 }
